b92a1eb79d20e07837a7c5c134c176edc72f8c78
Mussa <mussasaeed11@gmail.com>
2021-10-20 09:39:52 +1000

    added code

--- basic/lesson1.js
+++ basic/lesson1.js
@@ -1,1 +1,15 @@
 console.log('hello world')
+
+'let cant delete but can replace and add/ pop remove and push is add'
+
+let details= {'firstname': 'mussa', 'lastname': 'saeed'}
+let list= [1,2,3,4,5,6]
+details.push(age= 16)
+
+
+function convert(fahrenheit)  {
+    return (fahrenheit - 32) * 5/9
+}
+function description(fruits)  {
+    for(let fruit of fruits){console.log (fruit + ' is a fruit')  }
+}

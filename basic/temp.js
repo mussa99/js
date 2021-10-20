a1d0bb86ef8bae60ec19894f20347d7b2a0f1d4c
Mussa <mussasaeed11@gmail.com>
2021-10-20 11:46:21 +1000

    done basic function and if , else

--- basic/temp.js
+++ basic/temp.js
@@ -1,24 +1,40 @@
 // functions and if, else 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
 
-function convert(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9
-}
-rl.question('enter fahrenight degrees ', function (degrees) {
-    console.log(convert(degrees) + ' celcius is ' + degrees + ' fahrenight')
-    process.exit(0);
-})
+// function convert(fahrenheit) {
+//     return (fahrenheit - 32) * 5 / 9
+// }
+// rl.question('enter fahrenight degrees ', function (degrees) {
+//     console.log(convert(degrees) + ' celcius is ' + degrees + ' fahrenight')
+//     process.exit(0);
+// })
 
 //if and else
-f = 10
-if (f > 10) {
-    console.log(f + ' is greater than 10')
+
+function greater(a, b) {
+    if (a > b) {
+        console.log(a + ' is greater than ' + b)
+    }
+    else if (b > a) {
+        console.log(b + ' is greater than ' + a)
+    }
+    else {
+       console.log ('there both equal ')
+    }
 }
-else if (7 < 10) {
-    console.log('10 is greater or equal to ')
-}
+
+
+console.log(greater(8, 10))
+console.log(greater(3, 5))
+
+console.log(greater(-1, -10))
+
+console.log(greater(8.5, 100))
+
+console.log(greater(0, 0))
+

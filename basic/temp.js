64f49cfb68e008e871d7db3c4c9ac52c0ecf5daa
Mussa <mussasaeed11@gmail.com>
2021-10-20 10:32:08 +1000

    added input

---
+++ basic/temp.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+function convert(fahrenheit)  {
+    return (fahrenheit - 32) * 5/9
+}
+rl.question('enter fahrenight degrees ', function(degrees) {
+    console.log(convert(degrees)+  ' celcius is ' + degrees + ' fahrenight')
+    process.exit(0);
+})
